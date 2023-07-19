9d24ac63195aca0e47faefb2c67df4e9b8800606
mason tucker <mason@listella.com>
2023-07-19 19:03:57 -0400

    feat: use the fully qualified domain name instead of the transport's local address for raft.

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -88,7 +88,11 @@ func (a *Agent) setupLogger() error {
 }
 
 func (a *Agent) setupMux() error {
-	rpcAddr := fmt.Sprintf(":%d", a.Config.RPCPort)
+	addr, err := net.ResolveTCPAddr("tcp", a.Config.BindAddr)
+	if err != nil {
+		return err
+	}
+	rpcAddr := fmt.Sprintf("%s:%d", addr.IP.String(), a.Config.RPCPort)
 	ln, err := net.Listen("tcp", rpcAddr)
 	if err != nil {
 		return err
@@ -106,11 +110,16 @@ func (a *Agent) setupLog() error {
 		return bytes.Compare(b, []byte{byte(log.RaftRPC)}) == 0
 	})
 
+	var err error
 	logConfig := log.Config{}
 	logConfig.Raft.StreamLayer = log.NewStreamLayer(raftLn, a.Config.ServerTLSConfig, a.Config.PeerTLSConfig)
+	rpcAddr, err := a.Config.RPCAddr()
+	if err != nil {
+		return err
+	}
+	logConfig.Raft.BindAddr = rpcAddr
 	logConfig.Raft.LocalID = raft.ServerID(a.Config.NodeName)
 	logConfig.Raft.Bootstrap = a.Config.Bootstrap
-	var err error
 	a.log, err = log.NewDistributedLog(a.Config.DataDir, logConfig)
 	if err != nil {
 		return err
